316e5b59fbfe49d064f4ad55345c1e6bc70afead
singh8840 <126312808+singh8840@users.noreply.github.com>
2024-02-23 13:11:33 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,34 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        mapping={")": "(", "}": "{"' "]": "["]}
+
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+    def main():
+        test_cases= [
+            ("CTTTTT",True),
+            ("OOO", True),
+            ("Inputs", False)
+        ]
+
+        for s, expected_output in test_cases:
+            output= isValid(s)
+            if output == expected_output:
+                print(f"Input: {s} - Output: {output} - Test Passed")
+            else:
+                print(f"Input: {s} - Output: {output} - Test Failed")
+
+    if __name__ == " __main__":
+         main() 
     
 
 
