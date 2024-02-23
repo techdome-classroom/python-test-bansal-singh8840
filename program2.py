2582212566c475ae17b923e1e18782ffd2e7c221
singh8840 <126312808+singh8840@users.noreply.github.com>
2024-02-23 13:27:03 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,31 @@
 class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+    def romanToInt(s: str) -> int:
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'c':100, 'D': 500, 'M': 1000}
+        result=0
+        prev_value =0
+
+        for char in reversed(s):
+            value= roman_dict[char]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+      return result
+def main():
+    test_case =[
+           ("III", 3),
+           ("LVIII", 58),
+           ("MCMXCIV", 1994)
+]
+
+for s, expected_output in test_cases:
+    output = romanToInt(s)
+    if output == expected_output:
+        print(f"Input: {s} - Output: {output} -Test Passed")
+    else:
+        print(f"Input: {s} - Output: {output} -Test Failed")
+
+if __name__ == "__main__":
+    main()
